4eab5d44251f878ce9fa8744ce26b9ed297f21b7
XinHui <148975664+xinhui03@users.noreply.github.com>
2024-11-30 20:41:30 +0800

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $database= new mysqli("localhost","root","","wpdc");    //connection to SWL database
-
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
